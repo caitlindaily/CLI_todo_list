bb5454243ad4a8cf87f8de822894ab909ed120a7
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-20 11:03:07 -0500

    Formatted and added "Add item to beginning or end"

--- todo.php
+++ todo.php
@@ -2,11 +2,12 @@
 
 // Create array to hold list of todo items
 $items =[];
-
+//============================================================
 // List array items formatted for CLI
 function list_items($list)
 {  
     $result = '';
+
     foreach ($list as $key => $value) 
     {
         $key++;    
@@ -14,42 +15,36 @@ function list_items($list)
     }
     return $result;
 }
-    // Return string of list items separated by newlines.
-    // Should be listed [KEY] Value like this:
-    // [1] TODO item 1
-    // [2] TODO item 2 - blah
-    // DO NOT USE ECHO, USE RETURN
-
-//$list_items = list_items($items);
-// Get STDIN, strip whitespace and newlines, 
-// and convert to uppercase if $upper is true
+//===============================================================   
 function get_input($upper = false) //Whatever is typed in is true. Nothing typed in is defaulted to false.
 {   
     $result = trim(fgets(STDIN));
+
     return $upper ? strtoupper($result) : $result;
-}   //when user inputs, if it's true, make sure to capitalize it, not, just give the input
-    //THis will always make sure the input is capital though
+}   //When user inputs, if it's true, make sure to capitalize it, not, just give the input
+    //this will always make sure the input is capital though
+//================================================================
 function sort_items($items) {
        
-        echo '(A)-Z, (Z)-A, (O)rder Entered, (R)everse order entered :';
+    echo '(A)-Z, (Z)-A, (O)rder Entered, (R)everse order entered :';
        
-        $input = get_input(true);
+    $input = get_input(true);
 
-        if ($input == 'A'){
-            asort($items);
-        }elseif($input == 'Z'){
-            arsort($items); 
-        }elseif($input == 'O') {
-            ksort($items);
-        }elseif ($input == 'R') {
-            krsort($items);
-        }
-        return $items;
+    if ($input == 'A'){
+        asort($items);
+    }elseif($input == 'Z'){
+        arsort($items); 
+    }elseif($input == 'O') {
+        ksort($items);
+    }elseif ($input == 'R') {
+        krsort($items);
+    }
+    return $items;
 }
+//==================================================================
 
-
-// The loop!
-do {
+do 
+{
     // Echo the list produced by the function
     echo list_items($items);
 
@@ -57,15 +52,26 @@ do {
     echo '(N)ew item, (R)emove item, (S)ort, (Q)uit : ';
 
     // Get the input from user
-    // Use trim() to remove whitespace and newlines
     $input = get_input(TRUE);
 
     // Check for actionable input
-    if ($input == 'N') {
-        // Ask for entry
+    if ($input == 'N') 
+    {
         echo 'Enter item: ';
-        // Add entry to list array
-        $items[] = get_input();
+        
+        $input = get_input();
+        echo "Would you like to add to the (B)eginning or the (E)nd?";
+        
+        $location = get_input(TRUE);
+        if ($location == 'B') 
+        {
+            array_unshift($items, $input);
+        }   //Puts item at beginning of list
+        else 
+        {
+            array_push($items, $input);
+        }   //Puts item at end of list
+        
     } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
@@ -78,8 +84,7 @@ do {
         $items = sort_items($items);
         // print_r($items);
     }
-
-    // $items = array_values($items);
+  
 // Exit when input is (Q)uit
 } while ($input != 'Q');
 
